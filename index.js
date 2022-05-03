d15c39adc6a81c31030915f8da9c9f1edc20c767
AhmedSoli92 <ahmed.soli101@gmail.com>
2022-05-03 15:36:44 +0200

    connect to DB, add middleware

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const connectDB = require('./config/DBConn');
 
-
-dotenv.config();
 const app = express();
+dotenv.config();
+// middleware
+app.use(express.json())
+app.use(helmet());
+app.use(morgan('common'));
+// connect to mogoDB
+connectDB()
+
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port: ${PORT}` );
+
+mongoose.connection.once('open',()=>{
+    console.log("Connected to DB")
+    app.listen(PORT,()=>{
+        console.log(`Server running on port: ${PORT}` );
+    });
+    
 })
+
